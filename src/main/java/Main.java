fcb5382ff0f2735f56e3774af2810cacf17b947a
piotrpiedel <piotrpiedel96@gmail.com>
2020-01-28 23:43:22 +0100

    Added base models;

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,29 @@
-public class Main {
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.geojson.FeatureCollection;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static configuration.DataSource.MONTHS_ALL_URL;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        URL allMonthsUrl = null;
+        try {
+            allMonthsUrl = new URL(MONTHS_ALL_URL);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        try {
+            FeatureCollection featureCollection =
+                    new ObjectMapper().disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES).readValue(allMonthsUrl, FeatureCollection.class);
+            featureCollection.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
---
+++ src/main/java/client/HttpClient.java
@@ -0,0 +1,10 @@
+package client;
+
+public class HttpClient {
+
+//    CloseableHttpClient httpClient = HttpClients.createDefault();
+//    HttpGet httpget = new HttpGet(MONTHS_ALL_URL);
+//    HttpResponse httpresponse = httpClient.execute(httpget);
+
+
+}
