3694861f35c28d3006df11d019805112b35ef8ad
piotrpiedel <piotrpiedel96@gmail.com>
2020-01-28 23:43:22 +0100

    Add base models and clients
    Add  HttpClient
    Add dataSource configuration
    Add Earthquake, Geometry and Properties models
    Add libraries: jackson, geojson-jackson

---
+++ src/main/java/configuration/DataSource.java
@@ -0,0 +1,6 @@
+package configuration;
+
+public class DataSource {
+    public static final String URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/";
+    public static final String MONTHS_ALL_URL = URL + "summary/all_month.geojson";
+}
---
+++ src/main/java/models/Geometry.java
@@ -0,0 +1,27 @@
+package models;
+
+import com.fasterxml.jackson.annotation.JsonGetter;
+
+import java.util.List;
+
+public class Geometry {
+
+    private String type;
+    private List<Double> coordinates;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public List<Double> getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(List<Double> coordinates) {
+        this.coordinates = coordinates;
+    }
+}
