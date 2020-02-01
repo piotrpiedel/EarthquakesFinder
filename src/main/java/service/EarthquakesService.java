e5bd4af1910c157d7e1d7e531da5fcdf5f95ea6f
piotrpiedcom <piotr.piedel@comarch.com>
2020-02-01 16:27:18 +0100

    Added tests;

--- src/main/java/service/EarthquakesService.java
+++ src/main/java/service/EarthquakesService.java
@@ -17,17 +17,21 @@ public class EarthquakesService {
 
     EarthquakesApiClient earthquakesApiClient = new MonthAllEarthquakes();
 
-    public Map<EarthquakePlaceToCoordinates, Double> getTenNearestEarthquakesForMonth(Coordinates coordinates) {
+    public Map<EarthquakePlaceToCoordinates, Double> getNearestEarthquakesSortedByDistanceDistinct(Coordinates coordinates, int earthquakesToDisplay) {
         List<EarthquakePlaceToCoordinates> earthquakePlaceToCoordinates = mapFeaturesToEarthquakePlaceToCoordinatesList(earthquakesApiClient.getFeaturesFromEndpoint());
+
         List<EarthquakePlaceToCoordinates> filteredEarthquakes = filterEarthquakesWithCoordinatesDuplicates(earthquakePlaceToCoordinates);
-        Map<EarthquakePlaceToCoordinates, Double> earthQuakesToDistanceMap = EarthquakesUtil.mapEarthquakesListToEarthQuakesToDistanceMap(filteredEarthquakes, coordinates);
-        Map<EarthquakePlaceToCoordinates, Double> earthquakesSortedByDistance = MapUtil.sortByValue(earthQuakesToDistanceMap);
-        return MapUtil.getEntriesSortedByValue(earthquakesSortedByDistance, 10);
+
+        Map<EarthquakePlaceToCoordinates, Double> earthquakesSortedByDistance = MapUtil
+                .sortByValue(EarthquakesUtil
+                        .mapEarthquakesListToEarthQuakesToDistanceMap(filteredEarthquakes, coordinates));
+
+        return MapUtil.getEntriesSortedByValue(earthquakesSortedByDistance, earthquakesToDisplay);
     }
 
     public String getPrintableNearbyEarthquakes(Coordinates coordinates) {
         StringBuilder stringBuilder = new StringBuilder();
-        Map<EarthquakePlaceToCoordinates, Double> earthquakes = getTenNearestEarthquakesForMonth(coordinates);
+        Map<EarthquakePlaceToCoordinates, Double> earthquakes = getNearestEarthquakesSortedByDistanceDistinct(coordinates, 10);
         earthquakes.forEach((earthquake, distance) -> {
             if (stringBuilder.toString().isEmpty()) {
                 stringBuilder.append(earthquake.getPlace()).append(" || ").append(distance);
--- src/test/java/util/EarthquakesUtilTest.java
+++ src/test/java/util/EarthquakesUtilTest.java
@@ -1,21 +1,46 @@
 package util;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import domain.EarthquakePlaceToCoordinates;
+import org.geojson.Feature;
+import org.geojson.FeatureCollection;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class EarthquakesUtilTest {
 
+    List<Feature> featureList = null;
+    List<EarthquakePlaceToCoordinates> earthquakePlaceToCoordinates = null;
 
-    @Test
-    public void mapFeaturesToEarthquakePlaceToCoordinatesList() {
+    @Before
+    public void setUp() throws Exception {
+        featureList = new ObjectMapper()
+                .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+                .readValue(new FileReader("src/test/resources/jsonWithCoordinatesDuplicates.txt"), FeatureCollection.class)
+                .getFeatures();
+        earthquakePlaceToCoordinates = EarthquakesUtil.mapFeaturesToEarthquakePlaceToCoordinatesList(featureList);
+
     }
 
+    //File has 21 features so it should be mapped to 21 earthquakes
     @Test
-    public void mapEarthquakesListToEarthQuakesToDistanceMap() {
+    public void mapFeaturesToTheSameNumberOfEarthquakesPlacesToCoordinates() {
+        assertEquals(21, EarthquakesUtil.mapFeaturesToEarthquakePlaceToCoordinatesList(featureList).size());
     }
 
+    //File has 21 earthquakes and 3 duplicates so it should be 19 after filter
     @Test
-    public void filterEarthquakesWithCoordinatesDuplicates() {
+    public void filterEarthquakesWithCoordinatesFrom21WithDuplicatesTo19Unique() {
+        assertEquals(19, EarthquakesUtil.filterEarthquakesWithCoordinatesDuplicates(earthquakePlaceToCoordinates).size());
     }
 }
